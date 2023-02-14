5967aa397b7a47f99817e6c4c4f449c344336d4f
Robert Rogers <rerogers@gmail.com>
2023-02-14 17:18:46 -0600

    created a program with different variables

---
+++ VariableExercise/VariableExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿// declare and initialize variables for the following types:
+string myName = "Robert";
+int myAge = 41;
+char myMiddleInitial = 'E';
+bool isMarried = true;
+double avogadrosNumber = 6.022e23;
+decimal myDec = 1.2m;
+
+
+// my story involving all the variables
+Console.WriteLine($"My name is {myName} and I am {myAge} years old. My middle initial is {myMiddleInitial}. Am I married? {isMarried}.\n" +
+    $"Here is a number: {myDec}. This is Avogadro's Number: {avogadrosNumber}. \nHave a great day.");
